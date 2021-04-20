61159670dbdc244aa793da551514cb1f4afce8b5
poojan-bacancy <poojan.bhatt@bacancy.com>
2021-04-20 11:08:27 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <View>
+      <Text>THis</Text>
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
